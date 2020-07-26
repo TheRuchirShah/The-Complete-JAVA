de56bb5ee22f63ca2d9930a701018ba9cc2f8598
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-26 15:53:01 +0530

    Create RandomNumberExample3.java

---
+++ Basic/RandomNumberExample3.java
@@ -0,0 +1,27 @@
+// Random Number Generation in Java 8
+
+// In Java 8, a new method ints() has been added to the Random class. We must import the java.util.Random before using the method.
+
+
+import java.util.Random;   
+public class RandomNumberExample5   
+{   
+  public static void main(String[] args)   
+  {   
+    randomInts(5);  
+    randomInts(9, 50, 90);  
+    //getStreamOfRandomInts(30, 50);  
+  }   
+  //method that generates a stream of integers having size 5  
+  public static void randomInts(int num)   
+  {  
+    Random random = new Random();  
+    random.ints(num).forEach(System.out::println);  
+  }  
+  //method that generates a stream of 9 integers between 50 to 90  
+  public static void randomInts(int num, int origin, int bound)   
+  {  
+    Random random1 = new Random();  
+    random1.ints(num, origin, bound).forEach(System.out::println);  
+  }  
+}
