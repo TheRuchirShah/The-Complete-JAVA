eecb5a0965d2a0e31390436c9321eff4ee5faef0
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-26 15:24:11 +0530

    Create PalindromeExample.java

---
+++ Basic/PalindromeExample.java
@@ -0,0 +1,25 @@
+class PalindromeExample
+{  
+  public static void main(String args[])
+  {  
+    int r,sum=0,temp;    
+    int n=454;    //It is the number variable to be checked for palindrome  
+    
+    temp=n;    
+    
+    while(n>0)
+    {    
+      r=n%10;   // Remainder  
+      sum=(sum*10)+r;    
+      n=n/10;    
+    }    
+    if(temp==sum)    
+    {
+      System.out.println("Palindrome Number ");    
+    }
+    else    
+    {
+      System.out.println("Not Palindrome");    
+    }
+  }  
+}
