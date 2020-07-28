aa4047d51adb0353faf2a5bc4253c2c97c2b8bb6
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-28 18:00:58 +0530

    Update LeftTrianglePattern.java

--- Pattern/Star Pattern/LeftTrianglePattern.java
+++ Pattern/Star Pattern/LeftTrianglePattern.java
@@ -1,38 +1,35 @@
-public class LeftTrianglePattern  
-{    
+// Star Pattern
+// Left Triangle Star Pattern
+
+
+public class LeftTrianglePattern   
+{   
     public static void main(String args[])   
-    {    
+    {   
         //i for rows and j for columns      
         //row denotes the number of rows you want to print  
-        int i, j, row = 5;      //Outer loop work for rows  
-        for (i=0; i<row; i++)   
-        {  
-            //inner loop work for space      
-            for (j=2*(row-i); j>=0; j--)         
-            {  
-                //prints space between two stars      
-                System.out.print(" ");   
-            }   
-        //inner loop for columns  
-        for (j=0; j<=i; j++ )   
+        int i, j, row=5;    //outer loop for rows  
+    
+        for(i=0; i<row; i++)   
         {   
-        //prints star      
-        System.out.print("* ");   
+            for(j=0; j<=i; j++)   //inner loop for columns
+            {   
+                System.out.print("* ");    //prints stars
+            }
+    
+        System.out.println();   //throws the cursor in a new line after printing each line
         }   
-        //throws the cursor in a new line after printing each line  
-        System.out.println();   
-    }   
-    }   
+    }  
 } 
 
 
 /*
 OUTPUT :-
 
-           * 
-         * * 
-       * * * 
-     * * * * 
-   * * * * *
+*
+* *
+* * *
+* * * *
+* * * * * 
 
 */
