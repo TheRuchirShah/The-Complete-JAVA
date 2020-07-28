fe978af0303b35b0a810fbe4cf3d58d47230a3b3
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-28 17:46:23 +0530

    Update RandomNumberExample2.java

--- Basic/RandomNumberExample2.java
+++ Basic/RandomNumberExample2.java
@@ -7,20 +7,31 @@
 
 public class RandomNumberExample2  
 {  
-  public static void main( String args[] )   
-  {  
-    int min = 100;  
-    int max = 500;  
-    //Generate random double value from 200 to 400   
-    
-    System.out.println("Random value of type double between "+min+" to "+max+ ":");  
-    double a = Math.random()*(max-min+1)+min;   
-    System.out.println(a);  
-      
-    //Generate random int value from 200 to 400   
-    
-    System.out.println("Random value of type int between "+min+" to "+max+ ":");  
-    int b = (int)(Math.random()*(max-min+1)+min);  
-    System.out.println(b);  
-  }  
-} 
+    public static void main( String args[] )   
+    {  
+        int min = 100;  
+        int max = 500;  
+        //Generate random double value from 200 to 400   
+        
+        System.out.println("Random value of type double between "+min+" to "+max+ ":");  
+        double a = Math.random()*(max-min+1)+min;   
+        System.out.println(a);  
+        
+        //Generate random int value from 200 to 400   
+        
+        System.out.println("Random value of type int between "+min+" to "+max+ ":");  
+        int b = (int)(Math.random()*(max-min+1)+min);  
+        System.out.println(b);  
+    }  
+}
+
+
+/*
+OUTPUT :- 
+
+Random value of type double between 100 to 500:
+424.8060177597822
+Random value of type int between 100 to 500:
+207
+
+*/
