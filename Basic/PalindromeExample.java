1e1f47f27438f52f516cf14224833775932efc57
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-28 17:36:33 +0530

    Update PalindromeExample.java

--- Basic/PalindromeExample.java
+++ Basic/PalindromeExample.java
@@ -2,28 +2,36 @@
 // It can also be a string like LOL, MADAM, EVE etc.
 
 
-class PalindromeExample
+public class PalindromeExample
 {  
-  public static void main(String args[])
-  {  
-    int r,sum=0,temp;    
-    int n=454;    //It is the number variable to be checked for palindrome  
-    
-    temp=n;    
-    
-    while(n>0)
-    {    
-      r=n%10;   // Remainder  
-      sum=(sum*10)+r;    
-      n=n/10;    
-    }    
-    if(temp==sum)    
-    {
-      System.out.println("palindrome number ");    
-    }
-    else    
-    {
-      System.out.println("not palindrome");    
-    }
-  }  
+    public static void main(String args[])
+    {  
+        int r,sum=0,temp;    
+        int n=454;    //It is the number variable to be checked for palindrome  
+        
+        temp=n;    
+        
+        while(n>0)
+        {    
+            r=n%10;   // Remainder  
+            sum=(sum*10)+r;    
+            n=n/10;    
+        }    
+        if(temp==sum)    
+        {
+            System.out.println("It is a Palindrome Number ");    
+        }
+        else    
+        {
+            System.out.println("It is Not a Palindrome Number");    
+        }
+    }  
 }
+
+
+/*
+OUTPUT :-
+
+It is a Palindrome Number 
+
+*/
