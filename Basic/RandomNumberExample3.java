f9a1733e39cb607560f4e31ed57f479ec713f0f6
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-28 17:50:35 +0530

    Update RandomNumberExample3.java

--- Basic/RandomNumberExample3.java
+++ Basic/RandomNumberExample3.java
@@ -6,22 +6,45 @@
 import java.util.Random;   
 public class RandomNumberExample5   
 {   
-  public static void main(String[] args)   
-  {   
-    randomInts(5);  
-    randomInts(9, 50, 90);  
-    //getStreamOfRandomInts(30, 50);  
-  }   
-  //method that generates a stream of integers having size 5  
-  public static void randomInts(int num)   
-  {  
-    Random random = new Random();  
-    random.ints(num).forEach(System.out::println);  
-  }  
-  //method that generates a stream of 9 integers between 50 to 90  
-  public static void randomInts(int num, int origin, int bound)   
-  {  
-    Random random1 = new Random();  
-    random1.ints(num, origin, bound).forEach(System.out::println);  
-  }  
+    public static void main(String[] args)   
+    {   
+        randomInts(5);  
+        randomInts(9, 50, 90);  
+        //getStreamOfRandomInts(30, 50);  
+    }   
+    
+    //method that generates a stream of integers having size 5  
+    public static void randomInts(int num)   
+    {  
+        Random random = new Random();  
+        random.ints(num).forEach(System.out::println);  
+    }  
+    
+    //method that generates a stream of 9 integers between 50 to 90  
+    public static void randomInts(int num, int origin, int bound)   
+    {  
+        Random random1 = new Random();  
+        random1.ints(num, origin, bound).forEach(System.out::println);  
+    }  
 }
+
+
+/*
+OUTPUT :-
+
+1646905063
+-1420129667
+1853569416
+2009171277
+1257357491
+56
+86
+51
+76
+61
+70
+69
+60
+80
+
+*/
