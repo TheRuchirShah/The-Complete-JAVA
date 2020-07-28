3533154b0f183356a6a1acdbfe10445ad8f9d94a
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-07-28 17:43:37 +0530

    Update RandomNumberExample1.java

--- Basic/RandomNumberExample1.java
+++ Basic/RandomNumberExample1.java
@@ -6,12 +6,23 @@
 import java.lang.Math;   
 public class RandomNumberExample1  
 {   
-  public static void main(String args[])   
-  {   
-    // Generating random numbers  
-    System.out.println("1st Random Number: " + Math.random());   
-    System.out.println("2nd Random Number: " + Math.random());  
-    System.out.println("3rd Random Number: " + Math.random());   
-    System.out.println("4th Random Number: " + Math.random());   
-  }   
+    public static void main(String args[])   
+    {   
+        // Generating random numbers  
+        System.out.println("1st Random Number: " + Math.random());   
+        System.out.println("2nd Random Number: " + Math.random());  
+        System.out.println("3rd Random Number: " + Math.random());   
+        System.out.println("4th Random Number: " + Math.random());   
+    }   
 } 
+
+
+/*
+OUTPUT :-
+
+1st Random Number: 0.20236610715693215
+2nd Random Number: 0.1627458052530676
+3rd Random Number: 0.1506052814032245
+4th Random Number: 0.5068657392125097
+
+*/
