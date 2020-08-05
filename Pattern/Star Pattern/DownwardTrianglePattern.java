a95bef50953f188e024ffb37204f679703979cbb
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-08-05 19:11:25 +0530

    Create DownwardTrianglePattern.java

---
+++ Pattern/Star Pattern/DownwardTrianglePattern.java
@@ -0,0 +1,32 @@
+// Star Pattern
+// Star Downward Triangle Pattern
+
+
+public class DownwardTrianglePattern
+{  
+    public static void main(String[] args)
+    {
+        int rows=5;
+        for (int i= rows-1; i>=0 ; i--)     //inner loop
+        {
+            for (int j=0; j<=i; j++)        //outer loop
+            {
+                System.out.print("*" + " ");
+            }
+        //throws the cursor in the next line after printing each line
+        System.out.println();
+        }
+    }
+}
+
+
+/*
+OUTPUT :-
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
