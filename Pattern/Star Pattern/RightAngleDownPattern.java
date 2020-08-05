fa037ac3c2ae7ca89518a0ce0d8b573d946315e1
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-08-05 19:30:53 +0530

    Create RightAngleDownPattern.java

---
+++ Pattern/Star Pattern/RightAngleDownPattern.java
@@ -0,0 +1,35 @@
+// Star Pattern
+// Right Angle Downward Star Pattern
+
+
+public class RightAngleDownPattern  
+{  
+    public static void main(String args[])  
+    {  
+        int row=5;  
+        for (int i= row; i>= 1; i--)  
+        {  
+            for (int j=row; j>i;j--)  
+            {  
+                System.out.print(" ");  
+            }  
+            for (int k=1;k<=i;k++)  
+            {  
+                System.out.print("*");  
+            }  
+            System.out.println("");  
+        }  
+    }  
+}
+
+
+/*
+OUTPUT :-
+
+* * * * *
+  * * * *
+    * * * 
+      * *
+        *
+
+*/
