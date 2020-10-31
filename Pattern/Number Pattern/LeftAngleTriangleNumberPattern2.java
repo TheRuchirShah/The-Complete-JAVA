7f9a6908b51da7e53d301ac399a0cc23d6b18f0f
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-10-31 19:52:29 +0530

    Create LeftAngleTriangleNumberPattern2.java

---
+++ Pattern/Number Pattern/LeftAngleTriangleNumberPattern2.java
@@ -0,0 +1,34 @@
+// Number Pattern
+// Right Angle Triangle Number Pattern 2
+
+
+public class LeftAngleTriangleNumberPattern2
+{
+	public static void main(String[] args)   
+	{  
+		int i, j, k = 1;  
+		
+		for (i = 1; i <= 5; i++)	//inner loop  
+		{
+			for (j = 1; j< i + 1; j++)	//outer loop
+			{
+				//prints the value of k
+				System.out.print(k++ + " ");
+			}
+			System.out.println();	//throws the cursor at the next line
+		}
+	}
+}
+
+
+/*
+
+OUTPUT :-
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
