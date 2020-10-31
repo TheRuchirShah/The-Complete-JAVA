caafe6ad3718ed6edb40fc5d88bec6b799194381
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2020-10-31 19:56:20 +0530

    Create DiamondPattern.java

---
+++ Pattern/Number Pattern/DiamondPattern.java
@@ -0,0 +1,60 @@
+// Number Pattern
+// Diamond Pattern
+
+
+import java.util.Scanner;
+public class DiamondPattern
+{            
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            int n = 4;
+            for (int j = 1; j<= n - i; j++) 
+            { 
+                System.out.print("  "); 
+            } 
+            for (int k = i; k >= 1; k--)
+            {
+                 System.out.print(" " +k);
+            }
+            for (int l = 2; l <= i; l++) 
+            { 
+                System.out.print(" " +l); 
+            } 
+            System.out.println(); 
+        } 
+        for (int i = 3; i >= 1; i--)
+        {
+            int n = 3;
+            for (int j = 0; j<= n - i; j++) 
+            { 
+                System.out.print("  "); 
+            } 
+            for (int k = i; k >= 1; k--)
+            {
+                System.out.print(" " +k);
+            }
+            for (int l = 2; l <= i; l++)
+            {
+                System.out.print(" " +l);
+            }
+            System.out.println();
+        }
+       
+    }
+}
+
+
+/*
+OUTPUT :-
+
+      1
+    2 1 2
+  3 2 1 2 3
+4 3 2 1 2 3 4
+  3 2 1 2 3
+    2 1 2
+      1
+
+*/
